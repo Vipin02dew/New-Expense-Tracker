86d43246f32a3f31f03c4bb4f29b2edd1749157a
VIPIN DEWANGAN <136557980+Vipin02dew@users.noreply.github.com>
2025-10-15 23:24:07 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVOidp9zcUt2pRnOOfPvSaApmJ78iNMEE",
+    authDomain: "expense-tracker-013.firebaseapp.com",
+    projectId: "expense-tracker-013",
+    storageBucket: "expense-tracker-013.firebasestorage.app",
+    messagingSenderId: "611715770437",
+    appId: "1:611715770437:web:eb76d2e2f33844dfafdb2c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
